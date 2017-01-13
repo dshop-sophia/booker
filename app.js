80483bf8ec7bcfeef6c8f469c065b2f39aa217a2
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-13 22:08:23 +0100

    Update default response

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const botBuilder = require('claudia-bot-builder');
 
 module.exports = botBuilder(function (message) {
+
+  console.log(JSON.stringify(message));
+
   return 'The booker app got your message ! ' + message;
 });
