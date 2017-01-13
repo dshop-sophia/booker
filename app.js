d0185a41f22d0f60fab4b896a2d63e77f8c70f67
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-13 23:10:30 +0100

    handle /book

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ module.exports = botBuilder(function (message) {
   var sender = message.originalRequest.user_name;
   var command = message.originalRequest.command;
   var args = [];
+  
   if (message.text != "") {
     args = message.text.split(' ');
   }
-
-
-  console.log('sender: ' + sender +  ' command: ' + command + ' args: ' + JSON.stringify(args));
 
   return Handler.processCommand(command, args, sender).then(function(reply){
     return reply;
