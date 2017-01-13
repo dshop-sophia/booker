ae56cc3c060a08089cb247c669f82dcb13efe2fa
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-13 22:06:49 +0100

    Update default response

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const botBuilder = require('claudia-bot-builder');
-const excuse = require('huh');
 
 module.exports = botBuilder(function (message) {
-  return 'Thanks for sending ' + message.text +
-    'Your message is very important to us, but ' +
-    excuse.get();
+  return 'The booker app got your message ! ' + message;
 });
