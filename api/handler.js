76214ca79fd31f5b8dcbbbf539226ef3aceed756
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-13 16:47:29 +0100

    no ECmaScript 6

--- api/handler.js
+++ api/handler.js
@@ -1,6 +1,6 @@
-const apiai = require('apiai');
-const Promise = require('promise');
-const app = apiai("YOUR_ACCESS_TOKEN");
+var apiai = require('apiai');
+var Promise = require('promise');
+var app = apiai("YOUR_ACCESS_TOKEN");
 
 
 module.exports = {
