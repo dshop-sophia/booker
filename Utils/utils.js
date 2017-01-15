05176cc05e30f48d450ca4ba033fa11557d67cfb
Skander BM <skander.bmahmoud@gmail.com>
2017-01-15 22:34:21 +0100

    a valid date must be in the future

--- Utils/utils.js
+++ Utils/utils.js
@@ -2,14 +2,20 @@ const moment = require('moment');
 
 module.exports = {
   isValidPeriod: function(start, end){
-    if(!this.isValid(start) || !this.isValid(end)) {
+    if(!this.isValidDate(start) || !this.isValidDate(end)) {
       return false;
     }
 
     return moment(start).isBefore(end);
   },
 
-  isValid: function(date){
-    return moment(date).isValid();
+  isValidDate: function(date){
+    var d = moment(date);
+
+    return d.isValid() && this.isInFuture(d);
+  },
+
+  isInFuture: function(date){
+    return date.isAfter(moment());
   }
 }
