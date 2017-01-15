20240403e409b15316503ae825fdf4b564b98925
Skander BM <skander.bmahmoud@gmail.com>
2017-01-15 18:18:50 +0100

    validating date and period

--- Utils/utils.js
+++ Utils/utils.js
@@ -1,11 +1,15 @@
 const moment = require('moment');
 
 module.exports = {
-  isPeriod: function(start, end){
-    // TODO: use moment.js
+  isValidPeriod: function(start, end){
+    if(!this.isValid(start) || !this.isValid(end)) {
+      return false;
+    }
+
+    return moment(start).isBefore(end);
   },
 
   isValid: function(date){
-    // TODO: date is valid and is in the future
+    return moment(date).isValid();
   }
 }
